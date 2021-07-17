5b67e03443d863bd64ea50b7e8c4d41a71670c77
Chelsea Smith <smith2chelsea2@gmail.com>
2021-07-16 20:47:34 -0400

    basic about and nav

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Nav from './components/Nav'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      <div className="App">
+        <div className="about">
+          <h1 className="about-me">About Me</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus minus architecto quia quidem incidunt. Reiciendis quas architecto ipsa quam soluta officia quia? Eaque illo fugit aut culpa libero nesciunt beatae! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur et illum quaerat a aliquam! Quasi, quaerat laboriosam. Debitis inventore necessitatibus voluptas praesentium mollitia natus, pariatur omnis, nam suscipit nisi laborum?</p>
+        </div>
+      </div>
     </div>
+
   );
 }
 
