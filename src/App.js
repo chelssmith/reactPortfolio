eb5e20fc0e35e4e3e73ca4ca5c66e747cd1bf458
Chelsea Smith <smith2chelsea2@gmail.com>
2021-07-17 23:52:12 -0400

    app and compenent adjustments

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import './Style.css';
+
+
 import Nav from './components/Nav'
+import Contact from './components/Contact';
+import About from './components/About'
+import Projects from './components/Projects'
 
 
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <div className="App">
+    <BrowserRouter>
+      <div>
+        <Nav />
+        <Switch>
+          <Route path="/" component={About} exact />
+          <Route path="/contact" component={Contact} />
+          <Route path="/projects" component={Projects} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
+
+
+{/* <div className="App">
         <div className="about">
           <h1 className="about-me">About Me</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus minus architecto quia quidem incidunt. Reiciendis quas architecto ipsa quam soluta officia quia? Eaque illo fugit aut culpa libero nesciunt beatae! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur et illum quaerat a aliquam! Quasi, quaerat laboriosam. Debitis inventore necessitatibus voluptas praesentium mollitia natus, pariatur omnis, nam suscipit nisi laborum?</p>
         </div>
       </div>
-    </div>
-
-  );
-}
-
-export default App;
+    </div> */}
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,30 +1,38 @@
 import React from 'react';
 
-export default function Nav() {
-  const linkStyle = { border: '1px black', padding: '5px' };
+function Nav() {
+  
 
   return (
-    <nav className="main-header-menu">
-      <section
-        style={{
-          display: 'flex',
-          fontFamily: 'helvetica',
-          flexDirection: 'row',
-          alignItems: 'flex-start',
-          justifyContent: 'flex-start',
-        }}
-      >
-       
-        <div style={linkStyle}>
-          <a href="#">About</a>
-        </div>
-        <div style={linkStyle}>
-          <a href="#">Projects</a>
-        </div>
-        <div style={linkStyle}>
-          <a href="#">Contact</a>
-        </div>
-      </section>
-    </nav>
-  );
+    <div className="nav">
+      <NavLink to="/">About Me</NavLink>
+      <NavLink to="/projects">My Projects</NavLink>
+      <NavLink to="/contact">Contact</NavLink>
+    </div>
+
+
+    
+  )
 }
+// <nav className="main-header-menu">
+    //   <section
+    //     style={{
+    //       display: 'flex',
+    //       fontFamily: 'helvetica',
+    //       flexDirection: 'row',
+    //       alignItems: 'flex-start',
+    //       justifyContent: 'flex-start',
+    //     }}
+    //   >
+       
+    //     <div style={linkStyle}>
+    //       <a href="#">About</a>
+    //     </div>
+    //     <div style={linkStyle}>
+    //       <a href="#">Projects</a>
+    //     </div>
+    //     <div style={linkStyle}>
+    //       <a href="#">Contact</a>
+    //     </div>
+    //   </section>
+    // </nav>
